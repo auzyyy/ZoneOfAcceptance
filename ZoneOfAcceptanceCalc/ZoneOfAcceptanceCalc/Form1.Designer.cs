0a6fe143a2e44f6aee1fb3ee8915961fae81f0d0
austin wernli <auzy93wow@hotmail.com>
2014-03-10 17:36:14 -0600

    Some ranomd stuff done, trying to figure out how to display the values

--- ZoneOfAcceptanceCalc/ZoneOfAcceptanceCalc/Form1.Designer.cs
+++ ZoneOfAcceptanceCalc/ZoneOfAcceptanceCalc/Form1.Designer.cs
@@ -28,20 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox_Trials = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Probability = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_GenerateValues = new System.Windows.Forms.Button();
+            this.label_Error = new System.Windows.Forms.Label();
+            this.dataGridView_generatedValues = new System.Windows.Forms.DataGridView();
+            this.listView_generatedValues = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_generatedValues)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox_Trials
+            // 
+            this.textBox_Trials.Location = new System.Drawing.Point(98, 9);
+            this.textBox_Trials.Name = "textBox_Trials";
+            this.textBox_Trials.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Trials.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of trials";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fair probability";
+            // 
+            // textBox_Probability
+            // 
+            this.textBox_Probability.Location = new System.Drawing.Point(98, 35);
+            this.textBox_Probability.Name = "textBox_Probability";
+            this.textBox_Probability.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Probability.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 592);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(295, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Note: This application assumes fair probability of all outcomes";
+            // 
+            // button_GenerateValues
+            // 
+            this.button_GenerateValues.Location = new System.Drawing.Point(63, 74);
+            this.button_GenerateValues.Name = "button_GenerateValues";
+            this.button_GenerateValues.Size = new System.Drawing.Size(98, 23);
+            this.button_GenerateValues.TabIndex = 6;
+            this.button_GenerateValues.Text = "Generate Values";
+            this.button_GenerateValues.UseVisualStyleBackColor = true;
+            this.button_GenerateValues.Click += new System.EventHandler(this.button_GenerateValues_Click);
+            // 
+            // label_Error
+            // 
+            this.label_Error.AutoSize = true;
+            this.label_Error.ForeColor = System.Drawing.Color.Red;
+            this.label_Error.Location = new System.Drawing.Point(12, 531);
+            this.label_Error.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label_Error.Name = "label_Error";
+            this.label_Error.Size = new System.Drawing.Size(0, 13);
+            this.label_Error.TabIndex = 7;
+            // 
+            // dataGridView_generatedValues
+            // 
+            this.dataGridView_generatedValues.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_generatedValues.Location = new System.Drawing.Point(393, 9);
+            this.dataGridView_generatedValues.Name = "dataGridView_generatedValues";
+            this.dataGridView_generatedValues.Size = new System.Drawing.Size(240, 593);
+            this.dataGridView_generatedValues.TabIndex = 8;
+            // 
+            // listView_generatedValues
+            // 
+            this.listView_generatedValues.Location = new System.Drawing.Point(266, 9);
+            this.listView_generatedValues.Name = "listView_generatedValues";
+            this.listView_generatedValues.Size = new System.Drawing.Size(121, 580);
+            this.listView_generatedValues.TabIndex = 9;
+            this.listView_generatedValues.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1265, 614);
+            this.ClientSize = new System.Drawing.Size(645, 614);
+            this.Controls.Add(this.listView_generatedValues);
+            this.Controls.Add(this.dataGridView_generatedValues);
+            this.Controls.Add(this.label_Error);
+            this.Controls.Add(this.button_GenerateValues);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Probability);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Trials);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_generatedValues)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_Trials;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Probability;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_GenerateValues;
+        private System.Windows.Forms.Label label_Error;
+        private System.Windows.Forms.DataGridView dataGridView_generatedValues;
+        private System.Windows.Forms.ListView listView_generatedValues;
     }
 }
 
--- ZoneOfAcceptanceCalc/ZoneOfAcceptanceCalc/Form1.cs
+++ ZoneOfAcceptanceCalc/ZoneOfAcceptanceCalc/Form1.cs
@@ -12,9 +12,80 @@ namespace ZoneOfAcceptanceCalc
 {
     public partial class Form1 : Form
     {
+        private double expectedValue = 0;
+        private double acceptance = .5;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_GenerateValues_Click(object sender, EventArgs e)
+        {
+            int trials = GetTrialCount();
+            double probability = GetFairProbability();
+            expectedValue = trials * probability;
+            if (trials >= 0 && probability >= 0)
+            {
+                DisplayValues(trials, probability);
+            }
+        }
+
+        private void DisplayValues(int trials, double probability)
+        {
+            for (int i = 0; i < trials; i++)
+            {
+
+            }
+
+        }
+
+        private int GetTrialCount()
+        {
+            int trials = 0;
+            try
+            {
+                trials = int.Parse(textBox_Trials.Text);
+            }
+            catch (FormatException)
+            {
+                DisplayError("Please enter a valid number for trials");
+                return -1;
+            }
+            return trials;
+        }
+
+        private double GetFairProbability()
+        {
+            double probability = 0;
+            try
+            {
+                string[] parts = textBox_Probability.Text.Split('/');
+                if (parts.Length > 1)
+                {
+                    for (int i = 0; i < parts.Length; i++)
+                    {
+                        if ((i + 1) < (parts.Length))
+                        {
+                            double numerator = double.Parse(parts[i]); ;
+                            double denominator = double.Parse(parts[i + 1]);
+                            parts[i + 1] = (numerator / denominator) + "";
+                        }
+                    }
+                }
+                probability = double.Parse(parts[parts.Length - 1]);
+            }
+            catch (FormatException)
+            {
+                DisplayError("Please enter a valid number for probability");
+                return -1;
+            }
+            return probability;
+        }
+
+        private void DisplayError(string message)
+        {
+            label_Error.Text = message;
         }
     }
 }
